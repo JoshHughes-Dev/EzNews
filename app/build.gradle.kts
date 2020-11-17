58fbaa7a01e80750194cc230739abecd3375ea35
Josh <joshhughes.dev@gmail.com>
2020-11-17 12:34:18 +0000

    added core lib dependencies for Hilt/Dagger, Retrofit, Moshi and Accompanist. will add any more when needed.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     id("com.android.application")
     kotlin("android")
     kotlin("android.extensions")
+    kotlin("kapt")
+    id("dagger.hilt.android.plugin")
 }
 
 android {
@@ -22,8 +24,8 @@ android {
             isMinifyEnabled = true
             isShrinkResources = true
             proguardFiles(
-                    getDefaultProguardFile("proguard-android-optimize.txt"),
-                    "proguard-rules.pro"
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
             )
         }
     }
@@ -47,10 +49,11 @@ android {
 dependencies {
 
     implementation(Libs.Kotlin.stdlib)
+    implementation(Libs.Kotlin.Coroutines.android)
+
     implementation(Libs.AndroidX.coreKtx)
     implementation(Libs.AndroidX.appcompat)
     implementation(Libs.AndroidX.material)
-
     implementation(Libs.AndroidX.Compose.compiler)
     implementation(Libs.AndroidX.Compose.runtime)
     implementation(Libs.AndroidX.Compose.foundation)
@@ -59,11 +62,34 @@ dependencies {
     implementation(Libs.AndroidX.Compose.material)
     implementation(Libs.AndroidX.Compose.animation)
     implementation(Libs.AndroidX.Compose.materialIconsExtended)
-    implementation(Libs.AndroidX.Compose.tooling)
     implementation(Libs.AndroidX.Compose.runtimeLivedata)
+    implementation(Libs.AndroidX.Lifecycle.viewModelKtx)
+    implementation(Libs.AndroidX.UI.tooling)
 
+    implementation(Libs.Accompanist.insets)
+    implementation(Libs.Accompanist.coil)
+
+    implementation(Libs.Hilt.android)
+    implementation(Libs.Hilt.AndroidX.viewModel)
+    kapt(Libs.Hilt.compiler)
+    kapt(Libs.Hilt.AndroidX.compiler)
+
+    implementation(Libs.Moshi.moshi)
+    implementation(Libs.Retrofit.retrofit)
+    implementation(Libs.Retrofit.moshiConverter)
 
     testImplementation(Libs.JUnit.junit)
-    androidTestImplementation(Libs.AndroidX.Test.Ext.junit)
+
+    androidTestImplementation(Libs.JUnit.junit)
+    androidTestImplementation(Libs.AndroidX.Test.runner)
     androidTestImplementation(Libs.AndroidX.Test.espressoCore)
+    androidTestImplementation(Libs.AndroidX.Test.rules)
+    androidTestImplementation(Libs.AndroidX.Test.Ext.junit)
+    androidTestImplementation(Libs.Kotlin.Coroutines.test)
+    androidTestImplementation(Libs.AndroidX.UI.uiTest)
+    androidTestImplementation(Libs.Hilt.android)
+    androidTestImplementation(Libs.Hilt.AndroidX.viewModel)
+    androidTestImplementation(Libs.Hilt.testing)
+    kaptAndroidTest(Libs.Hilt.compiler)
+    kaptAndroidTest(Libs.Hilt.AndroidX.compiler)
 }
--- buildsrc/src/main/java/Dependencies.kt
+++ buildsrc/src/main/java/Dependencies.kt
@@ -9,6 +9,12 @@ object Libs {
         const val stdlib = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:$version"
         const val gradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:$version"
         const val extensions = "org.jetbrains.kotlin:kotlin-android-extensions:$version"
+
+        object Coroutines {
+            private const val version = "1.4.0"
+            const val android = "org.jetbrains.kotlinx:kotlinx-coroutines-android:$version"
+            const val test = "org.jetbrains.kotlinx:kotlinx-coroutines-test:$version"
+        }
     }
 
     object AndroidX {
@@ -28,18 +34,28 @@ object Libs {
             const val layout = "androidx.compose.foundation:foundation-layout:$version"
             const val ui = "androidx.compose.ui:ui:$version"
             const val material = "androidx.compose.material:material:$version"
-            const val materialIconsExtended = "androidx.compose.material:material-icons-extended:$version"
+            const val materialIconsExtended =
+                "androidx.compose.material:material-icons-extended:$version"
             const val animation = "androidx.compose.animation:animation:$version"
             const val runtimeLivedata = "androidx.compose.runtime:runtime-livedata:$version"
-            const val tooling = "androidx.ui:ui-tooling:$version"
             const val test = "androidx.compose.test:test-core:$version"
-            const val uiTest = "androidx.ui:ui-test:$version"
+        }
+
+        object Lifecycle {
+            private const val version = "2.3.0-beta01"
+            const val viewModelKtx = "androidx.lifecycle:lifecycle-viewmodel-ktx:$version"
+        }
+
+        object UI {
+            const val tooling = "androidx.ui:ui-tooling:${Compose.version}"
+            const val uiTest = "androidx.ui:ui-test:${Compose.version}"
         }
 
         object Test {
             private const val version = "1.2.0"
             const val core = "androidx.test:core:$version"
             const val rules = "androidx.test:rules:$version"
+            const val runner = "androidx.test:runner:$version"
 
             object Ext {
                 private const val version = "1.1.2-rc01"
@@ -48,6 +64,39 @@ object Libs {
 
             const val espressoCore = "androidx.test.espresso:espresso-core:3.3.0"
         }
+    }
+
+    object Hilt {
+        private const val version = "2.29.1-alpha"
+
+        const val gradlePlugin = "com.google.dagger:hilt-android-gradle-plugin:$version"
+        const val android = "com.google.dagger:hilt-android:$version"
+        const val compiler = "com.google.dagger:hilt-compiler:$version"
+        const val testing = "com.google.dagger:hilt-android-testing:$version"
+
+        object AndroidX {
+            private const val version = "1.0.0-alpha02"
+
+            const val compiler = "androidx.hilt:hilt-compiler:$version"
+            const val viewModel = "androidx.hilt:hilt-lifecycle-viewmodel:$version"
+        }
+    }
+
+    object Accompanist {
+        private const val version = "0.3.3.1"
+        const val coil = "dev.chrisbanes.accompanist:accompanist-coil:$version"
+        const val insets = "dev.chrisbanes.accompanist:accompanist-insets:$version"
+    }
+
+    object Moshi {
+        private const val version = "1.11.0"
+        const val moshi = "com.squareup.moshi:moshi:$version"
+    }
+
+    object Retrofit {
+        private const val version = "2.9.0"
+        const val retrofit = "com.squareup.retrofit2:retrofit:$version"
+        const val moshiConverter = "com.squareup.retrofit2:converter-moshi:$version"
     }
 
     object JUnit {
